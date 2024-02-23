db2ab17e0b4e40a9934334fcdbf6e0f4303052cf
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-02-23 16:40:55 +0100

    programma mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,14 +1,18 @@
-const listaMail= ['mariorossi@gmail.com','pietrobianchi@gmail.com', 'carloverdii@gmail.com', 'ludovicorossi@gmail.com']
-
+const listaMail= ['mariorossi@gmail.com','pietrobianchi@gmail.com', 'carloverdi@gmail.com', 'ludovicorossi@gmail.com']
 const emailUtente= prompt ('inserisci la tua mail')
-for(let i=0 ; i<listaMail.length; i++){
-if (listaMail[i]!=emailUtente) {
+
+for(let i=0 ; i<listaMail.length ; i++){
+const mail = listaMail[i];
+ console.log(mail)
+
+if (emailUtente === mail) {
   console.log(emailUtente)
   console.log(listaMail[i])
-  document.getElementById('messaggio').innerHTML='La tua mail non è presente nella lista '
+ 
+  document.getElementById('messaggio').innerHTML='La tua mail è presente nella lista '
 }
 else{
-  document.getElementById('messaggio').innerHTML='La tua mail è presente nella lista   '
+  document.getElementById('messaggio').innerHTML='La tua mail non è presente nella lista   '
 
 }
 }
