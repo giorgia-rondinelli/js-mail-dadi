1fa60bf03add6d312b12a0dac166bfd6788e265b
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-02-26 10:17:49 +0100

    correzione proramma mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,25 +1,32 @@
 const listaMail= ['mariorossi@gmail.com','pietrobianchi@gmail.com', 'carloverdi@gmail.com', 'ludovicorossi@gmail.com']
 const emailUtente= prompt ('inserisci la tua mail')
+let  messaggio= 'la tua mail non è sulla lista'
+let mailTrovata = false
 
-for(let i=0 ; i<listaMail.length ; i++){
+
+for(let i=0 ; i< listaMail.length ; i++){
 const mail = listaMail[i];
  console.log(mail)
- let falso= 'la tua mail non è presente nella lista '
- let vero='la tua mail è presente sulla lista'
-if (emailUtente === mail) {
- console.log(vero)
- const presente= 'presente'
- document.getElementById('messaggio').innerHTML='la tua mail è presente nella lista '
+
+
+if (mail ===emailUtente ) {
+ console.log('mail trovata')
+ mailTrovata= true
+
+
   
 }
 
-else{
-  console.log(falso)
-  const assente='assente'
-  document.getElementById('messaggio').innerHTML='la tua mail non è presente nella lista'
+}
 
+if (mailTrovata){
+  messaggio='la tua mail è sulla lista '
 }
-}
+document.getElementById('messaggio').innerHTML= messaggio
+console.log(mailtrovata)
+
+
+
 
 
 
